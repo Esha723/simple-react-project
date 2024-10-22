49889129e1ae070fe17e397fe72ab0954b1ecd8b
unknown <isratesha022@gmail.com>
2024-10-22 16:17:50 +0600

    done

--- src/pages/Home/Products.jsx
+++ src/pages/Home/Products.jsx
@@ -4,7 +4,7 @@ import second from "../../assets/images/card-2.jpg";
 const Products = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-8 place-content-center place-items-center py-14">
-      <div className="card bg-slate-200 w-96 shadow-xl">
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
         <figure>
           <img src={first} alt="Shoes" />
         </figure>
@@ -25,7 +25,8 @@ const Products = () => {
           </div>
         </div>
       </div>
-      <div className="card bg-slate-200 w-96 shadow-xl">
+
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
         <figure>
           <img src={second} alt="Shoes" />
         </figure>
@@ -46,7 +47,8 @@ const Products = () => {
           </div>
         </div>
       </div>
-      <div className="card bg-slate-200 w-96 shadow-xl">
+
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
         <figure>
           <img src={first} alt="Shoes" />
         </figure>
@@ -67,7 +69,8 @@ const Products = () => {
           </div>
         </div>
       </div>
-      <div className="card bg-slate-200 w-96 shadow-xl">
+
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
         <figure>
           <img src={second} alt="Shoes" />
         </figure>
@@ -88,7 +91,8 @@ const Products = () => {
           </div>
         </div>
       </div>
-      <div className="card bg-slate-200 w-96 shadow-xl">
+
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
         <figure>
           <img src={first} alt="Shoes" />
         </figure>
@@ -109,6 +113,29 @@ const Products = () => {
           </div>
         </div>
       </div>
+
+      <div className="card bg-slate-200 max-w-96 shadow-xl">
+        <figure>
+          <img src={second} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="flex justify-between items-center">
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+            <div className="card-actions">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 };
